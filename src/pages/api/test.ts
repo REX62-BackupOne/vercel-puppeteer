b23af17e268ae06c1278da582ed1fb964df57b16
REX62-BackupOne <160718954+REX62-BackupOne@users.noreply.github.com>
2024-08-28 21:17:36 +0530

    Add files via upload

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -21,41 +21,9 @@ export default async function handler(
         return res.status(400).json({ message: 'A ?url query-parameter is required' });
     }
 
-    function isPackageInNodeModules(packageName: string) {
-        // Construct the path to the package in node_modules
-        const packagePath = path.resolve(__dirname, '../../../../', 'node_modules', packageName);
-
-        // Check if the package directory exists
-        console.log(packagePath)
-        return fs.existsSync(packagePath);
-    }
-    function isFolderInPackage(packageName: string, folderPath: string) {
-        const packageFolderPath = path.resolve(__dirname, '../../../../', 'node_modules', packageName, folderPath);
-
-        try {
-            const stats = fs.statSync(packageFolderPath);
-            return stats.isDirectory();
-        } catch (err) {
-            return false; // Return false if the folder does not exist
-        }
-    }
-    const packageNames = ['puppeteer-extra', 'chrome-aws-lambda', 'puppeteer-extra-plugin-stealth'];
-    for (const packageName of packageNames) {
-        const isInstalled = isPackageInNodeModules(packageName);
-
-        console.log(`${packageName} is ${isInstalled ? 'installed' : 'not installed'}`);
-    }
-
-    const packageName = 'puppeteer-extra-plugin-stealth';
-    const filePath = 'evasions/chrome.app'; // or any specific file within the package
-    const fileExists = isFolderInPackage(packageName, filePath);
-
-    console.log(`File ${filePath} in ${packageName} is ${fileExists ? 'present' : 'missing'}`)
     try {
         const browser = await puppeteerExtra.launch({
-            args: chromium.args,
-            executablePath: await chromium.executablePath,
-            headless: chromium.headless,
+            headless: true,
         });
 
         const page = await browser.newPage();
