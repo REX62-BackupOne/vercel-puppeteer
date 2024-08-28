1ad22606b7717d9ca807f426ac46364b5784b6b9
REX62-BackupOne <160718954+REX62-BackupOne@users.noreply.github.com>
2024-08-28 19:41:14 +0530

    Update test.ts

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -3,8 +3,12 @@ import puppeteer from 'puppeteer-core';
 import chromium from 'chrome-aws-lambda';
 import puppeteerExtra from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+const fs = require('fs');
+const path = require('path');
 
 puppeteerExtra.use(StealthPlugin());
+
+
 
 export default async function handler(
     req: NextApiRequest,
@@ -17,12 +21,36 @@ export default async function handler(
         return res.status(400).json({ message: 'A ?url query-parameter is required' });
     }
 
+    function isPackageInNodeModules(packageName: string) {
+        // Construct the path to the package in node_modules
+        const packagePath = path.join(__dirname, 'node_modules', packageName);
+
+        // Check if the package directory exists
+        return fs.existsSync(packagePath);
+    }
+    function isFileInPackage(packageName: string, filePath: string) {
+        // Construct the path to the file in the package directory
+        const packageFilePath = path.join(__dirname, 'node_modules', packageName, filePath);
+
+        // Check if the file exists
+        return fs.existsSync(packageFilePath);
+    }
+    const packageNames = ['puppeteer-extra', 'chrome-aws-lambda', 'puppeteer-extra-plugin-stealth'];
+    for (const packageName of packageNames) {
+        const isInstalled = isPackageInNodeModules(packageName);
+
+        console.log(`${packageName} is ${isInstalled ? 'installed' : 'not installed'}`);
+    }
+
+    const packageName = 'puppeteer-extra-plugin-stealth';
+    const filePath = 'evasions/chrome.app'; // or any specific file within the package
+    const fileExists = isFileInPackage(packageName, filePath);
+
+    console.log(`File ${filePath} in ${packageName} is ${fileExists ? 'present' : 'missing'}`)
     try {
-        const expath = await chromium.executablePath
-        console.log(expath)
         const browser = await puppeteerExtra.launch({
             args: chromium.args,
-            executablePath: expath,
+            executablePath: await chromium.executablePath,
             headless: chromium.headless,
         });
 
